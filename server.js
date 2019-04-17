0404e9a42872ec1552f0c7d3a3fd572e5fb706b1
John Oro <johnmichaeloro@gmail.com>
2019-04-17 12:57:39 -0600

    space hw finished

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 const express = require('express');
 const app = express();
 
-// run `npm install` to install dependencies in package.json
+// run `npm install` to install dependencies in package.json CHECK
 
 // * Your mission is to complete the app
 // * The app will need routes for index and show
 // * The app will need views for index and show
-//
+
 // * Make it so you can click on a mission’s name on the index page, and be taken to that mission’s show page
+//Basically, I need a list of mission names with buttons that lead to the show pages, i.e. the objects listed individually
 // * Bonus: add images to the data and have them display
 // * Bonus: add static css to style the pages
 
@@ -64,12 +65,24 @@ const marsMissions = [
 // send data to 'missions/index.ejs' view
 // the view should display just the names of each mission
 // display the mission names as <li> in a <ul> with the class name "missions"
+//CHECK
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    'marsMissions': marsMissions
+  })
+})
 
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
 
-
+app.get('/:id', (req, res) => {
+  console.log(marsMissions[req.params.id]);
+  res.render('missions/show.ejs', {
+    'marsMissions': marsMissions[req.params.id]
+  })
+})
 
 // LISTENER
 app.listen(port, function() {
